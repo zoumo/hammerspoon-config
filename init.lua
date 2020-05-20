7f0f04d1d4af9863f5c1ebdf8d070651c8deb8df
zoumo <zhangjun.zoumo@bytedance.com>
2020-05-20 13:30:34 +0800

    feat: enable VolumeScroll spoon

--- init.lua
+++ init.lua
@@ -3,31 +3,50 @@ hs.hotkey.alertDuration = 0
 hs.hints.showTitleThresh = 0
 hs.window.animationDuration = 0
 
--- load spoons
-hs.loadSpoon("ModalMgr")
-
-hspoon_list = {"WinWin"}
-for _, v in pairs(hspoon_list) do
-    hs.loadSpoon(v)
-end
-
--- load modules
 local function ends_with(str, ending)
     return ending == "" or str:sub(-#ending) == ending
 end
 
-local iterFn, dirObj = hs.fs.dir(hs.fs.pathToAbsolute("~/.hammerspoon/modules"))
-if iterFn then
-    for file in iterFn, dirObj do
-        if ends_with(file, '.lua') then
-            local moduleName = file:sub(1, -(#".lua" + 1))
-            require("modules/" .. moduleName)
+local function walkAndLoad(path, suffix, loadFn)
+    local iterFn, dirObj = hs.fs.dir(hs.fs.pathToAbsolute(path))
+    if iterFn then
+        for file in iterFn, dirObj do
+            if ends_with(file, suffix) then
+                local name = file:sub(1, -(#suffix + 1))
+                loadFn(name)
+            end
+        end
+    else
+        print(string.format("The following error occurred: %s", dirObj))
+        os.exit(1)
+    end
+end 
+
+-- load spoons
+hs.loadSpoon("ModalMgr")
+
+walkAndLoad(
+    "~/.hammerspoon/Spoons", 
+    ".spoon", 
+    function(name)
+        if name ~= "ModalMgr" then
+            hs.loadSpoon(name)
         end
     end
-else
-    print(string.format("The following error occurred: %s", dirObj))
-    os.exit(1)
-end
+)
+
+-- load modules
+walkAndLoad(
+    "~/.hammerspoon/modules", 
+    ".lua", 
+    function(name)
+        require("modules/" .. name)
+    end
+)
 
 -- Finally we initialize ModalMgr supervisor
 spoon.ModalMgr.supervisor:enter()
+
+-- start volume scroll
+spoon.VolumeScroll:init()
+spoon.VolumeScroll:start()
